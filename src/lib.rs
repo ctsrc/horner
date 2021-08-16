48fb504b32d06004953d57f4d1ac73a128ef4f6c
Erik Nordstrøm <erik@nordstroem.no>
2021-08-16 02:31:46 +0200

    Rename function that returns Result to include prefix "try" in its name.

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,10 @@ pub enum PolynomialEvalError {
 /// Here are some examples demonstrating use of eval_polynomial:
 ///
 /// ```
-/// use horner::eval_polynomial;
+/// use horner::try_eval_polynomial;
 ///
 /// // Evaluating the polynomial 72𝑥²+81𝑥+99 with 𝑥 = 5
-/// let val = eval_polynomial(5, &[72, 81, 99]).unwrap();
+/// let val = try_eval_polynomial(5, &[72, 81, 99]).unwrap();
 ///
 /// // Traditional calculation.
 /// let trad = 72 * 5_i32.pow(2) + 81 * 5 + 99;
@@ -31,23 +31,23 @@ pub enum PolynomialEvalError {
 /// ```
 ///
 /// ```
-/// # use horner::eval_polynomial;
+/// # use horner::try_eval_polynomial;
 /// // Here we have the "polynomial" 42, which is to say, 42𝑥⁰. Evaluated with 𝑥 = 9000
-/// assert_eq!(Ok(42), eval_polynomial(9000, &[42]));
+/// assert_eq!(Ok(42), try_eval_polynomial(9000, &[42]));
 /// ```
 ///
 /// ```
-/// # use horner::eval_polynomial;
+/// # use horner::try_eval_polynomial;
 /// // 23𝑥⁹+0𝑥⁸+27𝑥⁷+0𝑥⁶-5𝑥⁵+0𝑥⁴+0𝑥³+0𝑥²+0𝑥ⁱ+0𝑥⁰
 /// // Written simply: 23𝑥⁹+27𝑥⁷-5𝑥⁵
 /// // Evaluated with 𝑥 = 99
 ///
-/// let val = eval_polynomial(99_i128, &[23, 0, 27, 0, -5, 0, 0, 0, 0, 0]).unwrap();
+/// let val = try_eval_polynomial(99_i128, &[23, 0, 27, 0, -5, 0, 0, 0, 0, 0]).unwrap();
 /// let trad = 23 * 99_i128.pow(9) + 27 * 99_i128.pow(7) - 5 * 99_i128.pow(5);
 ///
 /// assert_eq!(val, trad);
 /// ```
-pub fn eval_polynomial<T: MulAddAssign + Copy> (x: T, constants: &[T]) -> Result<T, PolynomialEvalError>
+pub fn try_eval_polynomial<T: MulAddAssign + Copy> (x: T, constants: &[T]) -> Result<T, PolynomialEvalError>
 {
   let (&k, constants) = constants.split_first().ok_or(PolynomialEvalError::CardinalityTooLow)?;
 
